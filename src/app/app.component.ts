c521451b28f175f76785eb45e8e397cc59f31a22
Bertrand <pascal.bertrand85@laposte.net>
2019-12-09 14:04:54 +0100

    first commit angular Book

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bookshelves';
+  constructor() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyAXdR-Q6N21p3Xjzb4AazZtWv4N4zF5rtA",
+      authDomain: "bookshelves-72278.firebaseapp.com",
+      databaseURL: "https://bookshelves-72278.firebaseio.com",
+      projectId: "bookshelves-72278",
+      storageBucket: "bookshelves-72278.appspot.com",
+      messagingSenderId: "462139031874",
+      appId: "1:462139031874:web:5ce1cf4a57be07edc388db"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  
+  }
 }
